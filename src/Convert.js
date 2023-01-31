38021420c02adb58cbef8d46a18e5df493cc029b
lamen31 <andreas.lamen@gmail.com>
2023-01-31 14:49:16 +0700

    handle range JSON, add image, generate header and footer, JSON editor

--- src/Convert.js
+++ src/Convert.js
@@ -1,19 +1,55 @@
-import React, { useState, useEffect } from "react";
-import { Worker, Viewer } from "@react-pdf-viewer/core";
+import React, { useState, useEffect, useCallback } from "react";
+import {
+  Worker,
+  Viewer,
+  ScrollMode,
+  DocumentAskPasswordEvent,
+  PasswordStatus,
+  PrimaryButton,
+  TextBox,
+} from "@react-pdf-viewer/core";
+import type { RenderProtectedViewProps } from "@react-pdf-viewer/core";
+// import { PasswordStatus, PrimaryButton, TextBox } from '@react-pdf-viewer/core';
+// import type { DocumentAskPasswordEvent } from "@react-pdf-viewer/core";
 import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 import jsPDF from "jspdf";
 import "jspdf-autotable";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
+import OpenPDF from "react-password-protected-pdf-viewer";
+import { JsonEditor as Editor } from "jsoneditor-react";
+import "jsoneditor-react/es/editor.min.css";
 import Base64 from "./Base64.json";
 import JSONtest from "./JSONtest.json";
 import JSONtestComplex from "./JSONtestComplex.json";
 import { Col, Container, FormGroup, Label, Row } from "reactstrap";
 import moment from "moment";
 import "moment-timezone";
-// var qpdf = require("node-qpdf");
 
-function Convert() {
+// const ProtectedView = ({ passwordStatus, verifyPassword }) => {
+//   const [password, setPassword] = React.useState('');
+//   const submit = (): void => verifyPassword(password);
+
+//   return (
+//       {/* Input to enter the password */}
+//       <TextBox
+//           placeholder={"Enter the password ..."}
+//           type="password"
+//           value={password}
+//           onChange={setPassword}
+//       />
+
+//       {/* Tell users if the password is incorrect */}
+//       {passwordStatus === PasswordStatus.WrongPassword && (
+//           <div>The password is invalid. Please try again!</div>
+//       )}
+
+//       {/* Submit the password */}
+//       <PrimaryButton onClick={submit}>Submit</PrimaryButton>
+//   );
+// };
+
+const Convert = () => {
   const [urlPDF, setUrlPDF] = useState();
   const [filenamePDF, setFilenamePDF] = useState();
   const [listFont, setListFont] = useState();
@@ -21,20 +57,27 @@ function Convert() {
   const [fontPDF, setFontPDF] = useState();
   const [imageb64, setImageb64] = useState();
   const [isImage, setIsImage] = useState(false);
-  const [imageType, setImageType] = useState();
+  const [isHeader, setIsHeader] = useState(false);
+  const [isFooter, setIsFooter] = useState(false);
+  const [jsonData, setJsonData] = useState();
   const [isViewPDF, setIsViewPDF] = useState(false);
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
   const pdf = new jsPDF("p", "pt", "a4");
 
+  const handleAskPassword = (e: DocumentAskPasswordEvent) => {
+    e.verifyPassword("The password goes here");
+    // console.log("document ask password event ", DocumentAskPasswordEvent);
+  };
+
   const handleChangeFontSize = (fontSize) => {
     pdf.setFontSize(parseInt(fontSize));
-    GeneratePDFv2();
+    GeneratePDFv2(jsonData, isHeader, isFooter);
   };
 
   const handleChangeFont = (font) => {
     setFontPDF(font);
     pdf.setFont(font, "normal");
-    GeneratePDFv2();
+    GeneratePDFv2(jsonData, isHeader, isFooter);
   };
 
   const handleChangeAttachment = async (attachment) => {
@@ -135,72 +178,69 @@ function Convert() {
     setUrlPDF(pdf.output("datauri", "filename"));
   };
 
-  const GeneratePDFv2 = () => {
-    let length = 0;
+  const GeneratePDFv2 = (data, isHeader, isFooter) => {
+    let length = 40;
     let initLength = 0;
-    length = length + 40;
-    pdf.text(200, length, JSONtestComplex.report.title);
     length = length + 40;
     initLength = length;
     pdf.text(50, length, "Date");
     pdf.text(120, length, ":");
-    pdf.text(140, length, JSONtestComplex.report.date);
+    pdf.text(140, length, data.date);
     length = length + 20;
     pdf.text(50, length, "Name");
     pdf.text(120, length, ":");
-    pdf.text(140, length, JSONtestComplex.report.client.contact.name);
+    pdf.text(140, length, data.client.contact.name);
     length = length + 20;
     pdf.text(50, length, "Phone");
     pdf.text(120, length, ":");
-    pdf.text(140, length, JSONtestComplex.report.client.contact.phone);
+    pdf.text(140, length, data.client.contact.phone);
     length = length + 20;
     pdf.text(50, length, "Email");
     pdf.text(120, length, ":");
-    pdf.text(140, length, JSONtestComplex.report.client.contact.email);
+    pdf.text(140, length, data.client.contact.email);
     length = length + 20;
     pdf.text(50, length, "Address");
     pdf.text(120, length, ":");
     pdf.text(
       140,
       length,
-      JSONtestComplex.report.client.contact.address.street +
+      data.client.contact.address.street +
         ", " +
-        JSONtestComplex.report.client.contact.address.city +
+        data.client.contact.address.city +
         ","
     );
     length = length + 20;
     pdf.text(
       140,
       length,
-      JSONtestComplex.report.client.contact.address.state +
-        " " +
-        JSONtestComplex.report.client.contact.address.zip
+      data.client.contact.address.state + " " + data.client.contact.address.zip
     );
 
-    if (JSONtestComplex.report.pages.length > 1) {
-      for (var i = 1; i < JSONtestComplex.report.pages.length; i++) {
+    if (data.pages.length > 1) {
+      for (var i = 1; i < data.pages.length; i++) {
         pdf.addPage();
       }
     }
     if (isImage) {
       pdf.addPage();
     }
-    JSONtestComplex.report.pages.forEach((pages) => {
+    data.pages.forEach((pages) => {
       if (pages.page === 1) {
         pdf.setPage(pages.page);
-        pdf.text(250, 820, "page " + pages.page);
+        // pdf.text(250, 820, "page " + pages.page);
         const columnsMain = Object.keys(pages.data);
-        const columns1 = Object.keys(pages.data.cashflow);
-        const columns2 = Object.keys(pages.data.networth);
+        const columns1 = Object.keys(pages.data.cashflow[0]);
+        const columns2 = Object.keys(pages.data.networth[0]);
         let rows1 = [];
-        let tmp1 = [pages.data.cashflow.income, pages.data.cashflow.expenses];
-        rows1.push(tmp1);
+        pages.data.cashflow.forEach((element) => {
+          let tmp1 = [element.income, element.expenses];
+          rows1.push(tmp1);
+        });
         let rows2 = [];
-        let tmp2 = [
-          pages.data.networth.assets,
-          pages.data.networth.liabilities,
-        ];
-        rows2.push(tmp2);
+        pages.data.networth.forEach((element) => {
+          let tmp2 = [element.assets, element.liabilities];
+          rows2.push(tmp2);
+        });
         length = length + 40;
         pdf.text(50, length, columnsMain[0] + ":");
         length = length + 20;
@@ -271,25 +311,25 @@ function Convert() {
         });
       } else if (pages.page === 2) {
         pdf.setPage(pages.page);
-        pdf.text(250, 820, "page " + pages.page);
+        // pdf.text(250, 820, "page " + pages.page);
         length = initLength;
         let columnsMain2 = Object.keys(pages.data);
-        let columns3 = Object.keys(pages.data.budget);
-        let columns4 = Object.keys(pages.data.insurance);
+        let columns3 = Object.keys(pages.data.budget[0]);
+        let columns4 = Object.keys(pages.data.insurance[0]);
         let rows3 = [];
-        let tmp3 = [
-          pages.data.budget.monthly_income,
-          pages.data.budget.monthly_expenses,
-          pages.data.budget.savings,
-        ];
-        rows3.push(tmp3);
+        pages.data.budget.forEach((element) => {
+          let tmp3 = [
+            element.monthly_income,
+            element.monthly_expenses,
+            element.savings,
+          ];
+          rows3.push(tmp3);
+        });
         let rows4 = [];
-        let tmp4 = [
-          pages.data.insurance.product,
-          pages.data.insurance.coverage,
-          pages.data.insurance.premium,
-        ];
-        rows4.push(tmp4);
+        pages.data.insurance.forEach((element) => {
+          let tmp4 = [element.product, element.coverage, element.premium];
+          rows4.push(tmp4);
+        });
         pdf.text(50, length, columnsMain2[0] + ":");
         pages.data.priorities.forEach((element) => {
           length = length + 20;
@@ -373,16 +413,37 @@ function Convert() {
       }
       if (isImage) {
         pdf.setPage(3);
-        pdf.text(250, 820, "page " + 3);
+        // pdf.text(250, 820, "page " + 3);
         length = initLength;
         pdf.addImage(imageb64, "JPEG", 30, length, 300, 0);
       }
     });
+
+    const pageCount = pdf.internal.getNumberOfPages();
+    const pageSize = pdf.internal.pageSize;
+    const pageWidth = pageSize.width ? pageSize.width : pageSize.getWidth();
+    const pageHeight = pageSize.height ? pageSize.height : pageSize.getHeight();
+    for (let i = 1; i <= pageCount; i++) {
+      pdf.setPage(i);
+      if (isHeader) {
+        const header = data.title;
+        pdf.text(50, 40, header, { baseline: "top" });
+      }
+      if (isFooter) {
+        const footer = `Page ${i} of ${pageCount}`;
+        pdf.text(
+          pageWidth / 2 - pdf.getTextWidth(footer) / 2,
+          pageHeight - 15,
+          footer,
+          { baseline: "bottom" }
+        );
+      }
+    }
     setUrlPDF(pdf.output("datauri", "filename"));
   };
 
   const Print = async (filename) => {
-    GeneratePDFv2();
+    GeneratePDFv2(jsonData, isHeader, isFooter);
     let name_file;
     if (filename) {
       name_file = filename;
@@ -392,15 +453,30 @@ function Convert() {
         moment(new Date()).tz("Asia/Jakarta").format("DD-MM-YYYY HH:mm:ss.SSS");
     }
     pdf.save(name_file + ".pdf");
+    encryptPDF(name_file);
+  };
+
+  const encryptPDF = (filename) => {
     // var options = {
     //   keyLength: 40,
     //   password: "007007007",
     // };
     // await qpdf.encrypt(
-    //   "C:/Users/user/Downloads/" + name_file + ".pdf",
+    //   "C:/Users/user/Downloads/" + filename + ".pdf",
     //   options,
-    //   "C:/Users/user/Downloads/" + name_file + "_encrypted.pdf"
+    //   "C:/Users/user/Downloads/" + filename + "_encrypted.pdf"
     // );
+    // const pdfDoc = new HummusRecipe(
+    //   "C:/Users/user/Downloads/" + filename + ".pdf",
+    //   "C:/Users/user/Downloads/" + filename + "_encrypted.pdf"
+    // );
+    // pdfDoc
+    //   .encrypt({
+    //     userPassword: "123",
+    //     ownerPassword: "123",
+    //     userProtectionFlag: 4,
+    //   })
+    //   .endPDF();
   };
 
   useEffect(() => {
@@ -432,6 +508,7 @@ function Convert() {
     setIsViewPDF(false);
     setListFont(pdf.getFontList());
     pdf.setFontSize(16);
+    setJsonData(JSONtestComplex.report);
   }, []);
 
   return (
@@ -524,7 +601,7 @@ function Convert() {
                         <input
                           name="attachment"
                           type={"file"}
-                          accept=".jpeg, .png"
+                          accept="image/*"
                           onChange={(event) => {
                             handleChangeAttachment(event.target.files[0]);
                           }}
@@ -532,6 +609,54 @@ function Convert() {
                       </FormGroup>
                     </Col>
                   </Row>
+                  {isViewPDF && (
+                    <div>
+                      <Row
+                        style={{
+                          display: "flex",
+                          columnGap: "20px",
+                          marginTop: "0.5rem",
+                        }}
+                      >
+                        <Col>
+                          <label>
+                            <input
+                              name="header"
+                              type={"checkbox"}
+                              checked={isHeader}
+                              onChange={(event) => {
+                                setIsHeader(event.target.checked);
+                                GeneratePDFv2(
+                                  jsonData,
+                                  event.target.checked,
+                                  isFooter
+                                );
+                              }}
+                            />
+                            HEADER
+                          </label>
+                        </Col>
+                        <Col>
+                          <label>
+                            <input
+                              name="footer"
+                              type={"checkbox"}
+                              checked={isFooter}
+                              onChange={(event) => {
+                                setIsFooter(event.target.checked);
+                                GeneratePDFv2(
+                                  jsonData,
+                                  isHeader,
+                                  event.target.checked
+                                );
+                              }}
+                            />
+                            FOOTER
+                          </label>
+                        </Col>
+                      </Row>
+                    </div>
+                  )}
                 </Col>
               </Row>
               <Row
@@ -557,7 +682,7 @@ function Convert() {
                   <button
                     onClick={() => {
                       setIsViewPDF(true);
-                      GeneratePDFv2();
+                      GeneratePDFv2(jsonData, isHeader, isFooter);
                     }}
                   >
                     GENERATE
@@ -569,17 +694,21 @@ function Convert() {
                   {isViewPDF && (
                     <div>
                       <Row>
-                        <h4 style={{ fontWeight: "bold" }}>JSON VIEW:</h4>
-                      </Row>
-                      <Row>
-                        <pre>{JSON.stringify(JSONtestComplex, null, 2)}</pre>
-                      </Row>
-                      <Row>
-                        <h4 style={{ fontWeight: "bold" }}>PDF VIEW:</h4>
+                        <Editor
+                          value={jsonData}
+                          onChange={(event) => {
+                            setJsonData(event);
+                            GeneratePDFv2(event, isHeader, isFooter);
+                          }}
+                        />
                       </Row>
                       <Row>
                         <Viewer
                           fileUrl={urlPDF}
+                          // renderProtectedView={(renderProps) => (
+                          //   <ProtectedView {...renderProps} />
+                          // )}
+                          onDocumentAskPassword={handleAskPassword}
                           plugins={[defaultLayoutPluginInstance]}
                         />
                       </Row>
@@ -594,6 +723,6 @@ function Convert() {
       </div>
     </React.Fragment>
   );
-}
+};
 
 export default Convert;
